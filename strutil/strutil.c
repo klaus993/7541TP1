ecd03969665e352eb6323b6defbbb6efb3e4164d
klaus993 <lungwitz.klaus@gmail.com>
2016-10-12 17:18:54 -0300

    falopeadas

--- strutil/strutil.c
+++ strutil/strutil.c
@@ -4,11 +4,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+size_t strlen(const char *s);
+
 char** split(const char* str, char sep) {
 	if (sep == '\0') {
 		return NULL;
 	}
+	//size_t strlen = strlen(str);
+	char **result;
 	size_t count = 0;
+	int i;
 	char *tmp = str;
 	while (*tmp) {
 		if (*tmp == sep) {
@@ -16,36 +21,53 @@ char** split(const char* str, char sep) {
 		}
 		tmp++;
 	}
-	printf("count = %d\n", count);
-
-	char **result = malloc(sizeof(char*) * count);
-
+	printf("count = %zu\n", count);
+	count++;
+	result = malloc(sizeof(char*) * (count));
+	for (i = 0; i < count; i++) {
+		result[i] = malloc(sizeof(char) * (count / 2));
+	}
+	int j = 0;
+	for (i = 0; i < count; i++) {
+		while (str[j] != sep && str[j] != '\0') {
+			// guardar caracter
+			//putchar(str[j]);
+			result[i][j] = str[j];
+			j++;
+		}
+		j++;
+		result[i][j] = '\0';
+		//putchar('-');
+	}
 
 	return result;
 }
 
 char* join(char** strv, char sep) {
-	return NULL;
+	return NULL	;
 }
 
 void free_strv(char* strv[]) {
-	return;
-}
-
-int main() {
-	char **strv = malloc(sizeof(char*) * 4);
-	strv[0] = strdup("abc");
-	strv[1] = strdup("def");
-	strv[2] = strdup("ghi");
-	strv[3] = NULL;
-	int i;
-	split("asd,asd,asd", ',');
-	for (i = 0; i < 3; i++) {
-		printf("%s\n", strv[i]);
-	}
-	for (i = 0; i < 3; i++) {
+	for (int i = 0; strv[i] != NULL; i++) {
 		free(strv[i]);
 	}
 	free(strv);
+}
+
+int main() {
+	//char **strv = malloc(sizeof(char*) * 4);
+	//strv[0] = strdup("abc");
+	//strv[1] = strdup("def");
+	//strv[2] = strdup("ghi");
+	//strv[3] = NULL;
+	int i;
+	char ** splited = split("asd,asd,asd", ',');
+	for (i = 0; i < 3; i++) {
+		printf("%s\n", splited[i]);
+	}
+	//for (i = 0; i < 3; i++) {
+	//	free(strv[i]);
+	//}
+	//free(strv);
 	return 0;
 }
